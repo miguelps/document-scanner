78052e50f7c64c0d252e1d6e24b2e937dd404f85
sanster <cwq1913@gmail.com>
2018-09-06 15:27:55 +0800

    init

--- scanner.py
+++ scanner.py
@@ -1,92 +1,90 @@
+import argparse
+import os
+
 import cv2
 import numpy as np
-import rect
-
-# add image here.
-# We can also use laptop's webcam if the resolution is good enough to capture
-# readable document content
-image = cv2.imread('../Downloads/temp-2.jpg')
-
-# resize image so it can be processed
-# choose optimal dimensions such that important content is not lost
-image = cv2.resize(image, (1500, 880))
-
-# creating copy of original image
-orig = image.copy()
-
-# convert to grayscale and blur to smooth
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-#blurred = cv2.medianBlur(gray, 5)
-
-# apply Canny Edge Detection
-edged = cv2.Canny(blurred, 0, 50)
-orig_edged = edged.copy()
-
-# find the contours in the edged image, keeping only the
-# largest ones, and initialize the screen contour
-(contours, _) = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-#x,y,w,h = cv2.boundingRect(contours[0])
-#cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),0)
-
-# get approximate contour
-for c in contours:
-    p = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.02 * p, True)
-
-    if len(approx) == 4:
-        target = approx
-        break
 
 
-# mapping target points to 800x800 quadrilateral
-approx = rect.rectify(target)
-pts2 = np.float32([[0,0],[800,0],[800,800],[0,800]])
+def rectify(h):
+    h = h.reshape((4, 2))
+    hnew = np.zeros((4, 2), dtype=np.float32)
 
-M = cv2.getPerspectiveTransform(approx,pts2)
-dst = cv2.warpPerspective(orig,M,(800,800))
+    add = h.sum(1)
+    hnew[0] = h[np.argmin(add)]
+    hnew[2] = h[np.argmax(add)]
 
-cv2.drawContours(image, [target], -1, (0, 255, 0), 2)
-dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+    diff = np.diff(h, axis=1)
+    hnew[1] = h[np.argmin(diff)]
+    hnew[3] = h[np.argmax(diff)]
 
-
-# using thresholding on warped image to get scanned effect (If Required)
-ret,th1 = cv2.threshold(dst,127,255,cv2.THRESH_BINARY)
-th2 = cv2.adaptiveThreshold(dst,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,11,2)
-th3 = cv2.adaptiveThreshold(dst,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,2)
-ret2,th4 = cv2.threshold(dst,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return hnew
 
 
-cv2.imshow("Original.jpg", orig)
-cv2.imshow("Original Gray.jpg", gray)
-cv2.imshow("Original Blurred.jpg", blurred)
-cv2.imshow("Original Edged.jpg", orig_edged)
-cv2.imshow("Outline.jpg", image)
-cv2.imshow("Thresh Binary.jpg", th1)
-cv2.imshow("Thresh mean.jpg", th2)
-cv2.imshow("Thresh gauss.jpg", th3)
-cv2.imshow("Otsu's.jpg", th4)
-cv2.imshow("dst.jpg", dst)
+def watch(img, name):
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(name, 1200, 800)
+    cv2.imshow(name, img)
+    cv2.waitKey()
 
-# other thresholding methods
-"""
-ret,thresh1 = cv2.threshold(dst,127,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(dst,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(dst,127,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(dst,127,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(dst,127,255,cv2.THRESH_TOZERO_INV)
 
-cv2.imshow("Thresh Binary", thresh1)
-cv2.imshow("Thresh Binary_INV", thresh2)
-cv2.imshow("Thresh Trunch", thresh3)
-cv2.imshow("Thresh TOZERO", thresh4)
-cv2.imshow("Thresh TOZERO_INV", thresh5)
-"""
+def main(args):
+    image = cv2.imread(args.img)
+    print("Origin size: %s" % str(image.shape))
+    image = cv2.resize(image, (1500, 880))
+    watch(image, 'Origin')
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    watch(gray, 'Gray')
 
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    # blurred = cv2.medianBlur(gray, 5)
+    watch(blurred, 'Blur')
+
+    edged = cv2.Canny(blurred, 0, 50)
+    watch(edged, 'Canny edged')
+
+    _, contours, _ = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    # 从大到小对轮廓排序
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    # x,y,w,h = cv2.boundingRect(contours[0])
+    # cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),0)
+
+    # 从轮廓获得近似多边形
+    for c in contours:
+        p = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * p, True)
+
+        if len(approx) == 4:
+            target = approx
+            break
+
+    # 做投影变换，摆正视图，目标视图的比例应该和文档的比例相关
+    approx = rectify(target)
+    pts2 = np.float32([[0, 0], [800, 0], [800, 800], [0, 800]])
+
+    M = cv2.getPerspectiveTransform(approx, pts2)
+    dst = cv2.warpPerspective(image, M, (800, 800))
+
+    cv2.drawContours(image, [target], -1, (0, 255, 0), 2)
+    dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+    watch(dst, 'Result')
+    watch(image, 'Image with contours')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img', default='./demo.jpg')
+    parser.add_argument('--mode', default='img', choices=['img', 'webcam'])
+
+    args = parser.parse_args()
+
+    if args.mode == 'img' and not os.path.exists(args.img):
+        parser.error("Image not exist.")
+
+    return args
+
+
+if __name__ == '__main__':
+    main(parse_args())
